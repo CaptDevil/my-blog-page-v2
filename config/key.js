7b7cff135fbe6524252037055ccdd5622392a620
CaptDevil <prabhudutta.sarangi@gmail.com>
2020-12-17 12:47:31 +0530

    New changes

--- config/key.js
+++ config/key.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 //Add your mongodb URL inside ""
-mongoose.connect("mongodb+srv://prabhu:prabhu123@cluster0.8o6bc.mongodb.net/blog_page?retryWrites=true&w=majority", { useNewUrlParser: true,useUnifiedTopology: true },(err)=>{
+// mongodb+srv://prabhu:ihuIN0CpPzYjG7fM@cluster0.8o6bc.mongodb.net/blog_page?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,useUnifiedTopology: true },(err)=>{
     if(err)
         throw err;
     console.log('MongoDB connected...');
